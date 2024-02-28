10eb855e4bab462058f42835cafb468396b64cb1
JohnBitu <bitujoaovictor@gmail.com>
2024-02-28 09:34:06 -0300

    adicionando as funções Async e Await e excluindo arquivos indesejaveis

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,21 @@
-const create = (req, res) => {
+const userService =  require('../services/users')
+
+const create = async (req, res) => {
     const {name, username, email, password, avatar, background} = req.body;
     
     if (!name || !username || !email || !password || !avatar || !background){
         res.status(400).send({ message: "Preencha todos os campos para cadastro"});
     }
+
+    const user = await userService.create(req.body);
+
+    if(!user){
+        return res.status(400).send({ message: "Erro na criação de usuário" });
+    }
+
     res.status(201).send({
         user: {
+            id: user._id,
             name,
             username,
             email,
