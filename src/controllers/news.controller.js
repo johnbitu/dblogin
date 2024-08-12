2cbb123777b5f2221ac467f48a30fca36ee137fb
JohnBitu <bitujoaovictor@gmail.com>
2024-08-12 17:43:45 -0300

    feat: Data pagination in the API

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -27,7 +27,27 @@ const create = async (req, res) => {
 }
 
 const findAll = async (req, res) => {
-    const news = await News.findAllNewsService();
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if(!limit){
+        limit = 5;
+    }
+
+    if(!offset){
+        offset = 0;
+    }
+
+    const news = await News.findAllNewsService(offset, limit);
+    const total = await News.countNews();
+    const currentUrl = req.baseUrl;
+
+
+    const next = offset + limit;
+    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset${offset}` : null ; // if ternario 
+
     if (news.length === 0) {
         return res.status(400).send({ message: "there is no news recorded" });
     }
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -2,6 +2,12 @@ const News = require('../models/News');
 
 const createService = (body) => News.create(body);
 
-const findAllNewsService = () => News.find();
+const findAllNewsService = (offset, limit) => News.find()
+.sort({_id: -1})
+.skip(offset)
+.limit(limit)
+.populate("user");
 
-module.exports = { createService, findAllNewsService };
+const countNews = () => News.countDocuments();
+
+module.exports = { createService, findAllNewsService, countNews };
